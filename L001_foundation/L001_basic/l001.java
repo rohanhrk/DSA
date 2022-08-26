4113424b078e875d69dd9b854f6d4c9a7a5dde60
rohan hazarika <rohanhrk1999@gmail.com>
2022-08-26 10:23:11 +0530

    coding

--- L001_foundation/L001_basic/l001.java
+++ L001_foundation/L001_basic/l001.java
@@ -47,16 +47,7 @@ public class l001 {
         }
     }
 
-    public static boolean isPrime_(int n) {
-        boolean prime = true;
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
-                prime = false;
-                break;
-            }
-        }
-        return prime;
-    }
+     
 
     public static void isPrime() {
         int n;
